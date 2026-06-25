d3f43c5552c72b37eac3427b1be5fcd6eff9feea
Lisa Depenne <lisadep17@gmail.com>
2026-06-25 17:35:34 +0200

    add Makefile and Libft in folder: includes

---
+++ Makefile
@@ -0,0 +1,61 @@
+#COMPILE_______________________
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -MMD -MP
+MAKEFLAGS += --no-print-directory
+
+NAME = pyramid
+
+#INCLUDES______________________
+INCLUDE = includes/
+
+#LIBFT
+INCLUDE_LIBFT = include/libft/
+LIBFT_A = include/libft/libft.a
+
+CFLAGS += -I$(INCLUDE) -I $(INCLUDE_LIBFT)
+
+BUILD_DIR = obj/
+SRC_DIR = src/
+
+#SRC___________________________
+
+SRC=main.c \
+	parse_arg.c
+
+#OBJ___________________________
+
+OBJ = $(SRC:%.c=$(BUILD_DIR)%.o)
+DEPS = $(SRC:%.c=$(BUILD_DIR)%.d)
+OBJ_DIR = $(sort $(shell dirname $(OBJ)))
+
+#_________________________________
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	@$(CC) $(CFLAGS) $^ -o $@
+	@echo "link Pyramide3D"
+
+$(LIBFT_A):
+	@make -C include/libft
+
+$(BUILD_DIR)%.o: $(SRC_DIR)%.c | $(OBJ_DIR)
+	@$(CC) $(CFLAGS) -c $< -o $@ -I$(INCLUDE)
+
+$(OBJ_DIR):
+	@mkdir -p $@
+
+clean:
+	@make -C include/libft/ clean
+	@rm -rf $(BUILD_DIR)
+	@echo "clean obj/"
+
+fclean: clean
+	rm -rf $(NAME)
+	@rm -f $(LIBFT_A)
+
+re: fclean all
+
+.PHONY: all clean fclean re
+
+-include $(DEPS)
---
+++ includes/libft/src/utils_bonus.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldepenne <ldepenne@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/04 09:51:23 by ldepenne          #+#    #+#             */
+/*   Updated: 2026/01/21 15:36:21 by ldepenne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst && del)
+	{
+		del(lst->content);
+		free(lst);
+	}
+}
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+	t_list	*tmp2;
+
+	if (!lst || !del)
+		return ;
+	tmp = (*lst);
+	while (tmp != NULL)
+	{
+		tmp2 = tmp;
+		tmp = tmp->next;
+		ft_lstdelone(tmp2, del);
+	}
+	*lst = NULL;
+}
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	while (lst && f)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	void	*tmp;
+	t_list	*newlst;
+	t_list	*newnode;
+
+	newlst = 0;
+	while (lst && f && del)
+	{
+		tmp = f(lst->content);
+		newnode = ft_newnode(tmp);
+		if (!newnode)
+		{
+			del(tmp);
+			ft_lstclear(&newlst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&newlst, newnode);
+		lst = lst->next;
+	}
+	return (newlst);
+}
