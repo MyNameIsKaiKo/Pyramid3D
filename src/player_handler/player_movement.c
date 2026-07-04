e50793744d873f92f186974165f6f9cd4eff0079
Jean-Baptiste Leray <jleray@3A5.42angouleme.fr>
2026-07-04 23:26:17 +0200

    Fixed the invisible wall !

--- src/player_handler/player_movement.c
+++ src/player_handler/player_movement.c
@@ -18,14 +18,16 @@ void	player_forward(t_map *map)
 	t_pos		*pos;
 	t_dir		*dir;
 	double		speed;
+	double		hitbox;
 
 	player = &map->player;
 	pos = &map->player.pos;
 	dir = &player->dir;
-	speed = player->velocity;
-	if (map->map_tab[(int)(pos->x + dir->x * speed)][(int)pos->y] == '0')
+	hitbox = 0.1;
+	speed = player->velocity + hitbox;
+	if (map->map_tab[(int)pos->y][(int)(pos->x + dir->x * speed)] == '0')
 		pos->x += dir->x * speed;
-	if (map->map_tab[(int)pos->x][(int)(pos->y + dir->y * speed)] == '0')
+	if (map->map_tab[(int)(pos->y + dir->y * speed)][(int)pos->x] == '0')
 		pos->y += dir->y * speed;
 	player->moves.forward = 0;
 }
@@ -36,14 +38,16 @@ void	player_backward(t_map *map)
 	t_pos		*pos;
 	t_dir		*dir;
 	double		speed;
+	double		hitbox;
 
 	player = &map->player;
 	pos = &player->pos;
 	dir = &player->dir;
-	speed = player->velocity;
-	if (map->map_tab[(int)(pos->x - dir->x * speed)][(int)pos->y] == '0')
+	hitbox = 0.1;
+	speed = player->velocity + hitbox;
+	if (map->map_tab[(int)pos->y][(int)(pos->x - dir->x * speed)] == '0')
 		pos->x -= dir->x * speed;
-	if (map->map_tab[(int)pos->x][(int)(pos->y - dir->y * speed)] == '0')
+	if (map->map_tab[(int)(pos->y - dir->y * speed)][(int)pos->x] == '0')
 		pos->y -= dir->y * speed;
 	player->moves.backward = 0;
 }
@@ -54,14 +58,16 @@ void	player_left(t_map *map)
 	t_pos		*pos;
 	t_plane		*plane;
 	double		speed;
+	double		hitbox;
 
 	player = &map->player;
 	pos = &player->pos;
 	plane = &player->plane;
-	speed = player->velocity;
-	if (map->map_tab[(int)(pos->x - plane->x * speed)][(int)pos->y] == '0')
+	hitbox = 0.1;
+	speed = player->velocity + hitbox;
+	if (map->map_tab[(int)pos->y][(int)(pos->x - plane->x * speed)] == '0')
 		pos->x -= plane->x * speed;
-	if (map->map_tab[(int)pos->x][(int)(pos->y - plane->y * speed)] == '0')
+	if (map->map_tab[(int)(pos->y - plane->y * speed)][(int)pos->x] == '0')
 		pos->y -= plane->y * speed;
 	player->moves.left = 0;
 }
@@ -72,14 +78,16 @@ void	player_right(t_map *map)
 	t_pos		*pos;
 	t_plane		*plane;
 	double		speed;
+	double		hitbox;
 
 	player = &map->player;
 	pos = &player->pos;
 	plane = &player->plane;
-	speed = player->velocity;
-	if (map->map_tab[(int)(pos->x + plane->x * speed)][(int)pos->y] == '0')
+	hitbox = 0.1;
+	speed = player->velocity + hitbox;
+	if (map->map_tab[(int)pos->y][(int)(pos->x + plane->x * speed)] == '0')
 		pos->x += plane->x * speed;
-	if (map->map_tab[(int)pos->x][(int)(pos->y + plane->y * speed)] == '0')
+	if (map->map_tab[(int)(pos->y + plane->y * speed)][(int)pos->x] == '0')
 		pos->y += plane->y * speed;
 	player->moves.right = 0;
 }
