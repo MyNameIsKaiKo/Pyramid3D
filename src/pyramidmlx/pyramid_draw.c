96cc978824e32d77551e2ae5b491b27c31116b59
Jean-Baptiste Leray <jleray@3A5.42angouleme.fr>
2026-07-05 01:32:02 +0200

    Create function for the base variable of the texture math

--- src/pyramidmlx/pyramid_draw.c
+++ src/pyramidmlx/pyramid_draw.c
@@ -45,8 +45,8 @@ void	exec_dda(t_map *map)
 			ray->map_y += ray->step_y;
 			ray->side = 1;
 		}
-		if (ray->map_x < 0 || ray->map_x >= map->width
-				|| ray->map_y < 0 || ray->map_y >= map->height)
+		if (ray->map_x < 0 || ray->map_x >= map->width || ray->map_y < 0
+			|| ray->map_y >= map->height)
 			break ;
 		if (map->map_tab[ray->map_y][ray->map_x] != '0')
 			ray->hit = 1;
---
+++ src/pyramidmlx/texture.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   texture.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jleray <marvin@d42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/05 01:13:49 by jleray            #+#    #+#             */
+/*   Updated: 2026/07/05 01:13:49 by jleray           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/pyramid_mlx.h"
+
+void	calc_wallx(t_map *map)
+{
+	t_player	*player;
+	t_ray		*ray;
+
+	player = &map->player;
+	ray = &player->ray;
+	if (ray->side == 0)
+		map->tex.wallx = player->pos.y + ray->wall_dist * ray->dir_y;
+	if (ray->side == 1)
+		map->tex.wallx = player->pos.x + ray->wall_dist * ray->dir_x;
+	map->tex.wallx = map->tex.wallx - (int)map->tex.wallx;
+}
+
+void	calc_tex_x(t_map *map)
+{
+	t_player	*player;
+	t_ray		*ray;
+	t_texture	*tex;
+
+	player = &map->player;
+	ray = &player->ray;
+	tex = &map->tex;
+	tex->x = (int)(tex->wallx * tex->witdh);
+	if (ray->side == 0 && ray->dir_x > 0)
+		tex->x = tex->witdh - tex->x - 1;
+	if (ray->side == 1 && ray->dir_y < 0)
+		tex->x = tex->witdh - tex->x - 1;
+}
+
+void	calc_tex_step(t_map *map)
+{
+	t_ray		*ray;
+	t_texture	*tex;
+
+	ray = &map->player.ray;
+	tex = &map->tex;
+	tex->step = (1.0 * tex->height) / ray->line_height;
+}
+
+void	calc_tex_start(t_map *map)
+{
+	t_ray		*ray;
+	t_texture	*tex;
+
+	ray = &map->player.ray;
+	tex = &map->tex;
+	tex->start = (ray->draw_start - (HEIGHT / 2) + (ray->line_height / 2))
+		* tex->step;
+}
