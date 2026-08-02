cb55f24af4d29778a48972164f19f15f7493f4c2
Lisa Depenne <lisadep17@gmail.com>
2026-08-02 15:00:03 +0200

    restore display_background

--- src/bonus/display_minimap_bonus.c
+++ src/bonus/display_minimap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ldepenne <ldepenne@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 10:36:27 by ldepenne          #+#    #+#             */
-/*   Updated: 2026/08/01 19:32:12 by ldepenne         ###   ########.fr       */
+/*   Updated: 2026/08/02 14:56:00 by ldepenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,35 +52,13 @@ static void	display_background(t_ctx *ctx, t_vec2 result, t_vec2 curr_block)
 {
 	int	y;
 	int	x;
-	t_img	tex;
-	t_img	minitex;
-	int		resize_x;
-	int		resize_y;
-	int		w;
-	int		h;
-
 
 	y = (int)(ctx->player.pos.y + result.y);
 	x = (int)(ctx->player.pos.x + result.x);
-	tex.img = mlx_xpm_file_to_image(ctx->mlx, ctx->tab_tex_bonus[B_F_FLOOR_COLOR], &w, &h);
-	tex.addr = mlx_get_data_addr(ctx->mlx, &tex.bits_per_pixel, &tex.line_lenght, &tex.endian);
-	minitex.img = mlx_new_image(ctx->mlx, 10, 10);
-	if (!ctx->img.img)
-	{
-		/** @todo change this: */
-		print_error("new_img");
-		free_ctx(ctx);
-		exit (1);
-	}
-	resize_x = x * w / 10;
-	resize_y = y * h / 10;
 	if (is_inmap(ctx, result) || ctx->map->map_tab[y][x] == EMPTY)
 		my_mlx_pixel_put(ctx, curr_block.x, curr_block.y, MCOLOR_VOID);
 	else if (ctx->map->map_tab[y][x] == FLOOR)
-	{
-		mlx_put_image_to_window(ctx->mlx, );
-		// my_mlx_pixel_put(ctx, curr_block.x, curr_block.y, MCOLOR_FLOOR);
-	}
+		my_mlx_pixel_put(ctx, curr_block.x, curr_block.y, MCOLOR_FLOOR);
 	else if (ctx->map->map_tab[y][x] == 'o')
 		my_mlx_pixel_put(ctx, curr_block.x, curr_block.y, MCOLOR_OPEN_DOOR);
 	else
